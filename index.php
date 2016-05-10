c6565a2c94e0374fb65f55ac1c1f175b2b5a0f0c
Axenu <simonseregon@gmail.com>
2016-05-10 22:17:35 +0200

    Initial structure created

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+  <title>Game house start</title>
 </head>
 <body>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph my first commit.second commit</p>
+  <a href="login.php">Login</a>
+  <h1>Game House</h1>
+  <p>Info text</p>
+  <a href="bookView.php">book</a>
 
+  <?php
+    $mysqli = new mysqli('localhost', "loadData", "yrEqRKBGvRHsBZ3P", "game_house");
+    // if ($mysqli->connect_error) {
+    //   echo "Connection failed: " . $conn->connect_error;
+    // }
+    if (($result = $mysqli->query("SELECT * FROM `reviews_house` LIMIT 5"))) {
+      while($obj = $result->fetch_object()) {
+        echo "<div class='review'>\n";
+        echo "<h3 class='reviewTitle'>".$obj->title."</h3>\n";
+        echo "<p class='reviewRating'>".$obj->rating."</p>\n";
+        echo "<p class='reviewContent'>".$obj->content."</p>\n";
+        echo "</div>\n";
+      }
+    } else {
+      echo "SELECT * FROM `reviews_house` LIMIT 5";
+    }
+  ?>
 
 </body>
 </html>
