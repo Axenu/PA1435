b9f5e4bbf80e917ac1dcd851dcf46741e4ab4173
Capelliexp <the.1man@hotmail.com>
2016-05-12 16:13:26 +0200

    test login

--- gamepage/gamepage.php
+++ gamepage/gamepage.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="css.css" type="text/css"/>
+  <link rel="stylesheet" href="../css.css" type="text/css"/>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>Site name</title>
 </head>
 <body>
   <div id="header">
+    <div id="LoginField">
+      Login
+    </div>
+    <q>Game House</q>
   </div>
 
   <div id="contain">
