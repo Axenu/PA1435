fe6917f38b681d465c4931ac6aa00d8b88e9af0a
Axenu <simonseregon@gmail.com>
2016-05-15 11:32:27 +0200

    browsing dates complete

--- bookingView.php
+++ bookingView.php
@@ -40,6 +40,39 @@ if (!isset($_GET['game'])) {
     echo"<a href='loginView.php'>Login</a>";
 
     echo"<div id='contain'>";
+
+    $mysqli = new mysqli('localhost', "loadData", "yrEqRKBGvRHsBZ3P", "game_house");
+    date_default_timezone_set('Europe/Stockholm');
+
+    if ($stmt = $mysqli->prepare("SELECT price FROM prices WHERE (month=".date('n')." AND hour=12)")) {
+        // $query = $_POST['query']."%";
+        // $stmt->bind_param('s', $query);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows < 5) {
+            $stmt->close();
+            //create data for current month
+            $days = cal_days_in_month(CAL_GREGORIAN, date('n'), date('Y'));
+            for ($i = 0; $i < $days; $i++) {
+                for ($j = 0; $j < 24; $j++) {
+                    $query = "INSERT INTO `prices` (month, day, hour, price, year) VALUES (".date('n').", ".($i+1).", ".($j+1).", 100, ".date('Y').")";
+                    if ($result = $mysqli->query($query)) {
+                    } else {
+                        echo $query;
+                    }
+                }
+            }
+        }
+        $stmt = $mysqli->prepare("SELECT day, price FROM prices WHERE (month=".date('n')." AND hour=12)");
+        $stmt->execute();
+        $stmt->bind_result($day, $price);
+        while ($stmt->fetch()) {
+          echo "<p onclick='showResults(this)'>day: ".$day." price: ".$price."</p>";
+        }
+      } else {
+        // echo "SELECT title FROM games WHERE title LIKE '".$_POST['query']."%' LIMIT 5";
+      }
+
     echo"</div>";
 
     echo"<?php getSearchView(); ?>";
@@ -49,8 +82,8 @@ if (!isset($_GET['game'])) {
     echo"<div id='footer'><p><center>";
     echo"Blekinge Institute of Technology  <b>|</b>  2016  <b>|</b>  PA1435, Objektorientad Programmering  <b>|</b>   Alfons Dahl, Simon Nilsson, Filip Pentikäinen</center><p>";
     echo"</div>";
-  echo"</body>";
-  echo"</html>";
+    echo"</body>";
+    echo"</html>";
 } else {
 
 }
