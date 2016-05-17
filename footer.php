0a2f94e37cd1a0fbec9fb6083b2a8780fe9660a3
Capelliexp <the.1man@hotmail.com>
2016-05-17 16:07:16 +0200

    efter footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
 
 </div>
-
-<<<<<<< HEAD
-<div id="footer">
-    <p>
-=======
-<div id="footer"><p class="footer">
->>>>>>> ecf91f2dbd75c2c104839cd2b20e9faaec1eac7c
+    <div id="footer"><p class="footer">
 Blekinge Institute of Technology  <b>|</b>  2016  <b>|</b>  PA1435, Objektorientad Programmering  <b>|</b>
 Alfons Dahl, Simon Nilsson, Filip Pentikäinen<p>
 </div>
